49abfc7e413238423b0b8ae59e53cc71ee491cc2
jetttech <ldjetton21@gmail.com>
2022-07-03 21:00:41 -0500

    switch to recommended nix setup

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 let
-  holonixPath = builtins.fetchTarball "https://github.com/holochain/holonix/archive/d492d68bdb9df1bf374dc68074f6d61b255988a3.tar.gz";
+  holonixPath = (import ./nix/sources.nix).holonix;
   holonix = import (holonixPath) {
     holochainVersionId = "custom";
     holochainVersion = import ./holochain_version.nix;
@@ -7,7 +7,8 @@ let
   nixpkgs = holonix.pkgs;
 in nixpkgs.mkShell {
   inputsFrom = [ holonix.main ];
-  packages = [
+  packages = with nixpkgs; [
+    niv
     nixpkgs.binaryen
   ];
 }
