9cc678fc7bdea83f9fc26a8f2da9a10c21864e21
jetttech <ldjetton21@gmail.com>
2022-07-14 09:33:25 -0500

    update nix update approach

--- default.nix
+++ default.nix
@@ -1,10 +1,6 @@
 let
   holonixPath = (import ./nix/sources.nix).holonix;
   holonix = import (holonixPath) {
-    rustVersion = {
-      track = "stable";
-      version = "1.60.0";
-    };
     holochainVersionId = "custom";
     holochainVersion = import ./holochain_version.nix;
   };
