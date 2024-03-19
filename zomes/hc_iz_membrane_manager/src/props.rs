98b6b69359b736b809b6cbad7f5d4b22f3d1b097
zo-el <joelulahanna@gmail.com>
2024-03-18 20:44:19 -0500

    debug

--- zomes/hc_iz_membrane_manager/src/props.rs
+++ zomes/hc_iz_membrane_manager/src/props.rs
@@ -10,7 +10,7 @@ pub struct Props {
 }
 
 pub fn holo_agent(encoded_props: &SerializedBytes) -> ExternResult<AgentPubKey> {
-    // trace!("encoded_props: {:?}", encoded_props);
+    info!("encoded_props: {:?}", encoded_props);
     let maybe_props = Props::try_from(encoded_props.to_owned());
     if let Ok(props) = maybe_props.clone() {
         info!("Props: {:?}", props);
@@ -18,7 +18,7 @@ pub fn holo_agent(encoded_props: &SerializedBytes) -> ExternResult<AgentPubKey> 
             return Ok(AgentPubKey::try_from(a).unwrap());
         }
     }
-    // trace!("Props retrieved: {:?}", maybe_props);
+    info!("Props retrieved: {:?}", maybe_props);
     Err(wasm_error!(WasmErrorInner::Guest(
         "Cannot fetch get a holo-agent-override".to_string()
     )))
